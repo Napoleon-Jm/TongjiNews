608ac8ae15d0fd6e3d78302d938b2db8b3d6dc9a
恺心 <jimin.wjm@alipay.com>
2017-03-01 21:11:54 +0800

    增加 Toolbar 的双击事件处理.

---
+++ app/src/main/java/com/tongji/wangjimin/tongjinews/view/MyToolbar.java
@@ -0,0 +1,47 @@
+package com.tongji.wangjimin.tongjinews.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import java.util.Date;
+
+/**
+ * Created by wangjimin on 17/3/1.
+ */
+
+public class MyToolbar extends Toolbar{
+
+    private long mTime;
+
+    public MyToolbar(Context context) {
+        super(context);
+    }
+
+    public MyToolbar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+//        return super.onTouchEvent(ev);
+        final int action = ev.getAction();
+        switch(action){
+            case MotionEvent.ACTION_DOWN:
+                final long time = new Date().getTime();
+                if(time - mTime < 230){
+                    Toast.makeText(getContext(), "Double click.", Toast.LENGTH_LONG).show();
+                    return true;
+                }
+                mTime = time;
+        }
+        return super.onTouchEvent(ev);
+    }
+}
