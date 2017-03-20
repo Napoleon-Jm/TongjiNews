d6e9c526ceb91d26a2fe8e21ce05990fb4451c78
恺心 <jimin.wjm@alipay.com>
2017-03-20 17:38:59 +0800

    增加侧划菜单

--- app/src/main/java/com/tongji/wangjimin/tongjinews/ImportNewsActivity.java
+++ app/src/main/java/com/tongji/wangjimin/tongjinews/ImportNewsActivity.java
@@ -1,11 +1,17 @@
 package com.tongji.wangjimin.tongjinews;
 
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.tongji.wangjimin.tongjinews.adapter.MainViewPagerAdapter;
 import com.tongji.wangjimin.tongjinews.fragment.DigestImageFragment;
@@ -17,6 +23,8 @@ public class ImportNewsActivity extends AppCompatActivity {
     private MyToolbar mToolbar;
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
+    private NavigationView mNav;
+    private DrawerLayout mDrawer;
     private MainViewPagerAdapter mAdapter;
 
     @Override
@@ -24,6 +32,8 @@ public class ImportNewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_news);
         /* work */
+        mNav = (NavigationView) findViewById(R.id.nav_importnews);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_importnews);
         mToolbar = (MyToolbar) findViewById(R.id.toolbar_importnews);
         setSupportActionBar(mToolbar);
         mViewPager = (ViewPager)findViewById(R.id.viewpager);
@@ -50,6 +60,9 @@ public class ImportNewsActivity extends AppCompatActivity {
         });
         mTabLayout = (TabLayout)findViewById(R.id.tablayout_importnews);
         mTabLayout.setupWithViewPager(mViewPager);
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_format_list_bulleted_white_24dp, null);
+        mToolbar.setNavigationIcon(drawable);
+//        mToolbar.setLogo(drawable);
         mToolbar.setDoubleClickListener(new MyToolbar.DoubleClickListener() {
             @Override
             public void onClick() {
@@ -69,5 +82,14 @@ public class ImportNewsActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
